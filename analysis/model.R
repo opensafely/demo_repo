9110f22dc15f5543d77e16eb40daa0b138fbdbab
Em Prestige <69582748+emprestige@users.noreply.github.com>
2026-01-08 16:00:16 +0000

    updates to model

--- analysis/model.R
+++ analysis/model.R
@@ -24,18 +24,63 @@ model_tidy <- model %>%
   tidy_add_term_labels(labels = c(age = "Age (Years)")) %>%
   tidy_remove_intercept() %>% 
   mutate(
+    var_label = if_else(var_label == "copd", "COPD", str_to_title(var_label)),
     label = case_when(
-      label %in% c("TRUE", "FALSE") ~ paste0(var_label, "(", label, ")"),
-      var_label == "year" ~ paste0("Study Year ", label),
-      TRUE ~ label)
+      label %in% c("TRUE", "FALSE") ~ paste0(var_label, " (", str_to_title(label), ")"),
+      var_label == "Year" ~ paste0("Study Year ", label),
+      TRUE ~ str_to_title(label))
   )
 
-# define the order for the 
+# define the order for the variables
+var_order <- c("Female", "Male", "Intersex", "Age (Years)",
+               "White", "Mixed", "Asian Or Asian British",
+               "Black Or Black British", "Other Ethnic Groups",
+               "5 (Least Deprived)", "4", "3", "2", "1 (Most Deprived)", 
+               "Unknown", "Asthma (False)", "Asthma (True)", "COPD (False)",
+               "COPD (True)", "Study Year 1", "Study Year 2")
 
 # create forest plot
-model_tidy %>% 
-  ggplot(aes(y = label, x = estimate, xmin = conf.low, xmax = conf.high)) +
+plot <- model_tidy %>% 
+  ggplot(aes(y = factor(label, levels = rev(var_order)), x = estimate,
+             xmin = conf.low, xmax = conf.high)) +
   geom_vline(xintercept = 0, linetype = 2) +
-  #coord_cartesian(xlim = c(-4, 4)) +
+  labs(y = "Parameter", x = "Estimate") +
   geom_pointrange(position = position_dodge(width = 0.75), size = 0.5) +
   theme_bw()
+
+# add shading
+dat_ribbon <- plot$data %>%
+  select(variable, label, var_nlevels) %>%
+  unique() %>%
+  mutate(
+    xmin = min(plot$data$conf.low, na.rm = T),
+    xmax = max(plot$data$conf.high, na.rm = T),
+    label = factor(label, levels = rev(var_order))
+  ) %>%
+  arrange(label) %>%
+  mutate(
+    yposition = seq_len(n()),
+    ymin = yposition - 0.5,
+    ymax = yposition + 0.5
+  ) %>%
+  # assign a group order based on where each variable first appears in the arranged data
+  mutate(group_id = match(variable, unique(variable))) %>%
+  mutate(fill = if_else(group_id %% 2 == 1, "a", "b"))
+
+dat_ribbon_long <- dat_ribbon %>%
+  pivot_longer(cols = c(xmin, xmax), values_to = "x",
+               names_to = "xmin_xmax") %>%
+  select(-xmin_xmax)
+
+plot <- plot +
+  geom_ribbon(
+    data = dat_ribbon_long,
+    aes(x = x, ymin = ymin, ymax = ymax, group = yposition, fill = fill),
+    inherit.aes = FALSE,
+    alpha = 0.2   # sets transparency for filled areas
+  ) +
+  scale_fill_manual(
+    values = c("a" = "transparent", "b" = "grey50"),
+    guide = "none"   # hide legend if not needed
+  )
+plot
