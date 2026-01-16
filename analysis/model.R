db317323abcadaaa4df3e695e6c94c5afd7296fd
Em Prestige <69582748+emprestige@users.noreply.github.com>
2026-01-16 11:15:04 +0000

    condense actions

--- analysis/model.R
+++ analysis/model.R
@@ -5,52 +5,54 @@ library(arrow)
 library(broom)
 library(broom.helpers)
 
-# get arguments from the command line
-args <- commandArgs(trailingOnly = TRUE)
+# use a function to model the dataset
+model_dataset <- function(period) {
 
-# define the period being studied
-if (length(args) == 0) {
-  # if there are no command line arguments (for local running)
-  period <- "pandemic"
-} else {
-  # if there are command line arguments (for CLI running or jobs server)
-  period <- args[[1]]
+  # import dataset
+  df <- read_feather(
+    here::here("output", paste0("dataset_processed_", period, ".arrow"))
+  ) %>%
+    # remove "unknown' categories for modelling
+    mutate(
+      across(where(is.character), ~na_if(., "unknown")),
+      # turn IMD into a factor
+      imd_quintile = relevel(factor(
+        imd_quintile, levels = c("5 (least deprived)", "4", "3", "2", "1 (most deprived)"),
+        ordered = FALSE), ref = "5 (least deprived)"
+      )
+    )
+
+  # model quantity
+  model <- lm(
+    salbutamol_quantity ~ age + sex + ethnicity + imd_quintile + asthma + copd + year,
+    data = df
+  )
+
+  # tidy the model
+  model_tidy <- model %>%
+    tidy_and_attach() %>% 
+    tidy_add_reference_rows() %>% 
+    tidy_add_estimate_to_reference_rows(conf.level = 0.95) %>% 
+    tidy_add_term_labels(labels = c(age = "Age (Years)")) %>%
+    tidy_remove_intercept() %>% 
+    mutate(
+      var_label = if_else(var_label == "copd", "COPD", str_to_title(var_label)),
+      label = case_when(
+        label %in% c("TRUE", "FALSE") ~ paste0(var_label, " (", str_to_title(label), ")"),
+        var_label == "Year" ~ paste0("Study Year ", label),
+        TRUE ~ str_to_title(label))
+    )
+
+  # save the tidied model
+  write_csv(model_tidy, here::here("output", paste0("model_results_", period, ".csv")))
+
 }
 
-# import dataset
-df <- read_feather(
-  here::here("output", paste0("dataset_processed_", period, ".arrow"))
-) %>%
-  # remove "unknown' categories for modelling
-  mutate(
-    across(where(is.character), ~na_if(., "unknown")),
-    # turn IMD into a factor
-    imd_quintile = relevel(factor(
-      imd_quintile, levels = c("5 (least deprived)", "4", "3", "2", "1 (most deprived)"),
-      ordered = FALSE), ref = "5 (least deprived)"
-    )
-  )
+## pre-pandemic period
+model_dataset("pre_pandemic")
 
-# model quantity
-model <- lm(
-  salbutamol_quantity ~ age + sex + ethnicity + imd_quintile + asthma + copd + year,
-  data = df
-)
+## pandemic period
+model_dataset("pandemic")
 
-# tidy the model
-model_tidy <- model %>%
-  tidy_and_attach() %>% 
-  tidy_add_reference_rows() %>% 
-  tidy_add_estimate_to_reference_rows(conf.level = 0.95) %>% 
-  tidy_add_term_labels(labels = c(age = "Age (Years)")) %>%
-  tidy_remove_intercept() %>% 
-  mutate(
-    var_label = if_else(var_label == "copd", "COPD", str_to_title(var_label)),
-    label = case_when(
-      label %in% c("TRUE", "FALSE") ~ paste0(var_label, " (", str_to_title(label), ")"),
-      var_label == "Year" ~ paste0("Study Year ", label),
-      TRUE ~ str_to_title(label))
-  )
-
-# save the tidied model
-write_csv(model_tidy, here::here("output", paste0("model_results_", period, ".csv")))
+## post-pandemic period
+model_dataset("post_pandemic")
